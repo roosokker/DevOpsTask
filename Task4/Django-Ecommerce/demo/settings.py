1f0ce00aab5365b03d9fc7f6289960aad29236cb
roosokkar <rodainasokkar@gmail.com>
2025-11-02 19:05:53 +0200

    testing CI/CD

--- Task4/Django-Ecommerce/demo/settings.py
+++ Task4/Django-Ecommerce/demo/settings.py
@@ -4,7 +4,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '')
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
